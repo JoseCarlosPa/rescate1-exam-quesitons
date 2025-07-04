0b7091e97a5da218e3144ebf572e8b3000541936
josecarlospas <jcpachecordz@gmail.com>
2025-07-04 13:44:56 -0600

    feat(domain): new domain

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {HashRouter} from 'react-router'
+import {BrowserRouter} from 'react-router'
 import Router from "./components/Router/Router.component.tsx";
 import {Toaster} from "sonner";
 import Providers from "./Providers/Providers.component.tsx";
 
 createRoot(document.getElementById('root')!).render(
-    <HashRouter>
+    <BrowserRouter>
         <Providers>
             <Toaster richColors closeButton/>
             <Router/>
         </Providers>
-    </HashRouter>
+    </BrowserRouter>
 )
